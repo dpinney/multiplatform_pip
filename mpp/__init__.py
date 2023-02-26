449f26eaf2a42a4d95a38159ad5f06c03fb702ba
David Pinney <david@pinney.org>
2023-02-26 17:19:09 +0000

    sketching out the test structure

--- mpp/__init__.py
+++ mpp/__init__.py
@@ -1,1 +1,3 @@
-pass
+from .lib import numpy
+
+
--- mpp/lib/numpy.py
+++ mpp/lib/numpy.py
@@ -1,3 +1,12 @@
 import platform
 
-pass
+my_plat = platform.system()
+
+if my_plat == 'Linux':
+    pass
+elif my_plat == 'Windows':
+    pass
+elif my_play == 'Darwin': #macOS
+    pass
+else:
+    print(f'Your platform was detected as {my_plat} but we currently only support Linux/Windows/Darwin')
