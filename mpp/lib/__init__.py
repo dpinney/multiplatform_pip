8683773e41e1ef9a7cd1f4ca6779a16df1879bb9
David Pinney <david@pinney.org>
2023-03-17 14:57:53 +0000

    adding windows binaries

--- mpp/lib/__init__.py
+++ mpp/lib/__init__.py
@@ -1,13 +1,15 @@
-import os as _os, sys as _sys
+import os as _os, sys as _sys, platform as _platform
 
 this_dir = _os.path.dirname(__file__)
 py_bin = _sys.executable
 
 def install():
+    # sys_plat = platform.system()
     # my_plat = 'manylinux2014_x86_64'
-    # install_string = f'{py_bin } -m pip install --target="{this_dir}" --platform={my_plat} --only-binary=:all: numpy'
-    install_string = f'{py_bin } -m pip install --target="{this_dir}/Linux" --only-binary=:all: numpy'
-    _os.system(install_string)
+    # my_plat = 'win32'
+    # install_string = f'{py_bin } -m pip install --target="{this_dir}/{}" --platform={my_plat} --only-binary=:all: numpy'
+    install_string = f'{py_bin } -m pip install --target="{this_dir}/Linux/" --only-binary=:all: numpy'
+    # _os.system(install_string)
 
 if __name__ == '__main__':
 	install()
