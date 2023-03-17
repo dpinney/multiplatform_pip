a1fd2c5bc10de72f2538633ee12aa0f092a8d9a3
David Pinney <david@pinney.org>
2023-03-17 15:02:03 +0000

    enable tests on linux

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-pass
+from setuptools import setup, find_packages
+
+setup(
+    name='mpp',
+    version='1.0.0',
+    url='https://github.com/dpinney/multiplatform_pip',
+    author='David Pinney',
+    author_email='none@none.com',
+    description='Example of a python package with multiplatform statically linked libraries.',
+    packages=find_packages(),    
+    install_requires=[],
+)
