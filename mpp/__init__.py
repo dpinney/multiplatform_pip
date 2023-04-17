4a113d605251b08e953522bc7069196594703efa
David Pinney <david@pinney.org>
2023-04-17 21:44:01 +0000

    alternate approach with first time setup

--- mpp/__init__.py
+++ mpp/__init__.py
@@ -1,26 +1,29 @@
-import sys
-import os
-import platform
+import os as _os, sys as _sys, platform as _platform
+this_dir = _os.path.dirname(__file__)
+py_bin = _sys.executable
 
-# global things to know
-this_dir = os.path.dirname(__file__)
-my_plat = platform.system()
+try:
+    from mpp.lib import numpy as np
+except:
+    print('mpp: requirements missing. performing first time setup.')
+    install_string = f'{py_bin } -m pip install --target="{this_dir}/lib/" numpy'
+    _os.system(install_string)
 
-# set up libraries
-sys.path.insert(0,this_dir + '/lib/')
-if my_plat not in ['Linux', 'Windows', 'Darwin']:
-    print(f'Your platform was detected as {my_plat} but we currently only support Linux/Windows/Darwin')
-print(f'Platform detected: {my_plat}')
-sys.path.insert(0,this_dir + f'/lib/{my_plat}')
-print(f'New path: {sys.path}')
+# old approach please ignore.
+# this_dir = os.path.dirname(__file__)
+# my_plat = platform.system()
+# sys.path.insert(0,this_dir + '/lib/')
+# if my_plat not in ['Linux', 'Windows', 'Darwin']:
+#     print(f'Your platform was detected as {my_plat} but we currently only support Linux/Windows/Darwin')
+# print(f'Platform detected: {my_plat}')
+# sys.path.insert(0,this_dir + f'/lib/{my_plat}')
+# print(f'New path: {sys.path}')
 
 # how about a pure python lib?
 import flask
 
-# platform-specific imports
-# import numpy as np
-
 # some simple numpy test code
-# arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# print(arr2d)
-# print(arr2d[0, 1])
+print('numpy test:')
+arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr2d)
+print(arr2d[0, 1])
